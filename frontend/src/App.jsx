dfeab4a96ec8c4e46ab0388987d0811cd2f41ea4
Snehasish Mandal <mandalsneh97@gmail.com>
2024-11-26 16:50:46 +0530

    Updated the Routes of Browserouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,25 +20,23 @@ import Shop from "./pages/Shop";
 
 function App() {
   return (
-    <HashRouter>
-      <div className="h-screen p-2 relative">
-        <MainHeader />
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="*" element={<NoPage />} />
-        </Routes>
-        <Footer />
-      </div>
-    </HashRouter>
+    <div className="h-screen p-2 relative">
+      <MainHeader />
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
