8e266221abe3d74d658f3f8c6eab29dd4010a6fc
Snehasish Mandal <mandalsneh97@gmail.com>
2024-11-26 17:12:19 +0530

    Production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,4 @@
-import {
-  Routes,
-  Route,
-  HashRouter,
-  BrowserRouter as Router,
-} from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import MainHeader from "./components/header";
 import Register from "./pages/Register";
 import Navbar from "./components/navbar";
@@ -20,23 +15,25 @@ import Shop from "./pages/Shop";
 
 function App() {
   return (
-    <div className="h-screen p-2 relative">
-      <MainHeader />
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="*" element={<NoPage />} />
-      </Routes>
-      <Footer />
-    </div>
+    <Router>
+      <div className="h-screen p-2 relative">
+        <MainHeader />
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
